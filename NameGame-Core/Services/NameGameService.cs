6a5182e492418fe39aceea7d56813892b7f0a18c
Benjamin Micucci <Benjamin Micucci>
2016-10-18 17:45:54 -0400

    Modified Namegame as a best effort.

--- NameGame-Core/Services/NameGameService.cs
+++ NameGame-Core/Services/NameGameService.cs
@@ -1,26 +1,48 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using WillowTree.NameGame.Core.Models;
 
 namespace WillowTree.NameGame.Core.Services
 {
     public interface INameGameService
     {
-        Task<Person[]> GetPeople();
+        Task<List<Person>> GetPeople();
     }
 
     public class NameGameService : INameGameService
     {
         private static readonly string DataUrl = "http://api.namegame.willowtreemobile.com";
 
-        public async Task<Person[]> GetPeople()
+        /// <summary>
+        /// Retrieves the people from http://api.namegame.willowtreemobile.com and deserializes them 
+        /// into a List<Person>
+        /// </summary>        
+        public async Task<List<Person>> GetPeople()
         {
-            return null;
+            List<Person> people = new List<Person>();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(DataUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = Task.Run(() => client.GetAsync(client.BaseAddress)).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                people = JsonConvert.DeserializeObject<List<Person>>(jsonString);                
+            }
+            else
+            {
+                throw new HttpRequestException(string.Format("Namegame API responed with unsuccessful response code: {0}",
+                        response.StatusCode));
+            }
+
+            return people;
         }
     }
 }
